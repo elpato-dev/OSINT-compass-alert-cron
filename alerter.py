7496046232d8fd75b16154c5749f108772135285
Her0 <her0marodeur@protonmail.com>
2023-04-22 10:18:02 +0200

    fixed some async stuff with chatgpt, error can be ignored

--- alerter.py
+++ alerter.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+import asyncio
 
 #import modules
 from postgreser import get_alerts
@@ -21,31 +22,32 @@ rows = get_alerts()
 5 contact_details
 '''
 
-## for every row it checks if criteria is met and sends alert accordingly
-for row in rows:
+async def main():
+    for row in rows:
+        term = row[1]
 
-    term = row[1]
+        ##request to compass API 
+        compass_response = requests.get("https://osint-compass-api.onrender.com/term?term=" + term + "&apikey=" + api_key)
+        compass_json = compass_response.json()
+        twitter_sentiment = round(compass_json["tweets"]["sentiment"],3)
+        news_sentiment = round(compass_json["news"]["sentiment"], 3)
 
-    ##request to compass API 
-    compass_response = requests.get("https://osint-compass-api.onrender.com/term?term=" + term + "&apikey=" + api_key)
-    compass_json = compass_response.json()
-    twitter_sentiment = round(compass_json["tweets"]["sentiment"],3)
-    news_sentiment = round(compass_json["news"]["sentiment"], 3)
+        if row[2] != None:
+            scorelt = row[2]
+            if twitter_sentiment < scorelt or news_sentiment < scorelt:
+        
+                message = "Alert was triggered for term: '"+ term + "' the sentiment score is lower than " + str(scorelt) +" Twitter score is:" + str(twitter_sentiment) + " and news score is:" + str(news_sentiment)
 
-    if row[2] != None:
-        scorelt = row[2]
-        if twitter_sentiment < scorelt or news_sentiment < scorelt:
-    
-            message = "Alert was triggered for term: '"+ term + "' the sentiment score is lower than " + str(scorelt) +" Twitter score is:" + str(twitter_sentiment) + " and news score is:" + str(news_sentiment)
+                if row[4] == "telegram":
+                    await send_telegram(message, row[5])
+        
+        if row[3] != None:
+            scoregt = row[3]
+            if twitter_sentiment > scoregt or news_sentiment > scoregt:
+        
+                message = "Alert was triggered for term: '"+ term + "' the sentiment score is greater than " + str(scoregt) +" Twitter score is:" + str(twitter_sentiment) + " and news score is:" + str(news_sentiment)
 
-            if row[4] == "telegram":
-                send_telegram(message, row[5])
-    
-    if row[3] != None:
-        scoregt = row[3]
-        if twitter_sentiment > scoregt or news_sentiment > scoregt:
-    
-            message = "Alert was triggered for term: '"+ term + "' the sentiment score is greater than " + str(scoregt) +" Twitter score is:" + str(twitter_sentiment) + " and news score is:" + str(news_sentiment)
+                if row[4] == "telegram":
+                    await send_telegram(message, row[5])
 
-            if row[4] == "telegram":
-                send_telegram(message, row[5])
+asyncio.run(main())
--- telegramer.py
+++ telegramer.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from telegram import Bot
+from telegram import Bot, Message
 
 # Load environment variables from .env file
 load_dotenv()
@@ -8,10 +8,12 @@ load_dotenv()
 # Get Telegram bot token from environment variable
 bot_token = os.getenv("TELEGRAM_BOT_TOKEN")
 
-def send_telegram(message, chatid):
-   # Create bot instance
+async def send_telegram(message, chatid):
+    # Create bot instance
     bot = Bot(token=bot_token)
 
-
     # Send message
-    bot.send_message(chat_id=chatid, text=message)
+    sent_message = await bot.send_message(chat_id=chatid, text=message)
+    
+    # Return the message object
+    return sent_message
