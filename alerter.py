01618e6b5fb6d5c7e02b89c40842ced36a30435e
MTAttic <13680882+MTAttic@users.noreply.github.com>
2023-04-23 12:17:41 +0200

    Add common words to alert

--- alerter.py
+++ alerter.py
@@ -30,11 +30,17 @@ async def send_alerts():
         twitter_sentiment = round(compass_json["tweets"]["sentiment"],3)
         news_sentiment = round(compass_json["news"]["sentiment"], 3)
 
+        common_words_str = ""
+        for word, count in compass_json["common_words"]:
+            common_words_str += f"{word} ({count}), "
+
+        common_words_str = common_words_str[:-2]
+
         if row[2] != None:
             scorelt = row[2]
             if twitter_sentiment < scorelt or news_sentiment < scorelt:
         
-                message = "\u2757 Alert was triggered for term: <b>" + term + "</b>! The sentiment score is lower than " + str(scorelt) + ". Twitter score is: " + str(twitter_sentiment) + " and news score is: " + str(news_sentiment) + "."
+                message = "\u2757 Alert was triggered for term: <b>" + term + "</b>! The sentiment score is lower than " + str(scorelt) + ". Twitter score is: " + str(twitter_sentiment) + " and news score is: " + str(news_sentiment) + ".<br>The most common words used were: " + common_words_str + "."
 
                 if row[4] == "telegram":
                     await send_telegram(message, row[5])
@@ -43,7 +49,7 @@ async def send_alerts():
             scoregt = row[3]
             if twitter_sentiment > scoregt or news_sentiment > scoregt:
         
-                message = "\u2757 Alert was triggered for term: <b>" + term + "</b>! The sentiment score is greater than " + str(scoregt) + ". Twitter score is: " + str(twitter_sentiment) + " and news score is: " + str(news_sentiment) + "."
+                message = "\u2757 Alert was triggered for term: <b>" + term + "</b>! The sentiment score is greater than " + str(scoregt) + ". Twitter score is: " + str(twitter_sentiment) + " and news score is: " + str(news_sentiment) + ".<br>The most common words used were: " + common_words_str + "."
 
                 if row[4] == "telegram":
                     await send_telegram(message, row[5])
